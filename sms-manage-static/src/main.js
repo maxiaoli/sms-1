9802e6e06ab3f35b56888396a70e1ba2c6c0a378
leon_zhangxf <leonzhangxf@gmail.com>
2018-08-27 18:40:24 +0800

    增加elementui 和 vuex

--- sms-manage-static/src/main.js
+++ sms-manage-static/src/main.js
@@ -1,11 +1,14 @@
 import Vue from 'vue'
 import router from './router'
 import App from './App'
+import store from './store'
+import './plugins/element.js'
 
 Vue.config.productionTip = false;
 Vue.config.devtools = true;
 
 new Vue({
   render: h => h(App),
+  store,
   router
 }).$mount('#app');
